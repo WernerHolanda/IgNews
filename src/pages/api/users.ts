e232984144fb5b74ea37045b586f4dadf12ad371
WernerHolanda <dvaice.werner@gmail.com>
2023-07-06 20:01:04 -0300

    starting using backend on nextjs

---
+++ src/pages/api/users.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+export default (request: NextApiRequest, response:NextApiResponse) => {
+    const users = [
+        { id: 1, name: 'Diego'},
+        { id: 2, name: 'Dani'},
+        { id: 3, name: 'Rafa'},
+        { id: 4, name: 'Werner'},
+    ]
+
+    return response.json(users)
+}
